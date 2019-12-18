3cb3c1f0957d22e2f9b5b6934f0bf2e81cc3f2a2
Ross Mitchell <ross@edmondsommerce.co.uk>
2019-12-18 16:33:14 +0000

    Updating the qa runner

--- bin/qa.m2.bash
+++ bin/qa.m2.bash
@@ -7,13 +7,87 @@ set -o pipefail
 standardIFS="$IFS"
 IFS=$'\n\t'
 
-base_dir="${DIR}/../"
-directoryToTest="${base_dir}app/code/EdmondsCommerce"
+bin_dir=${DIR}
 
+base_dir="${DIR}/../../"
 
-php $base_dir/vendor/bin/phpcpd ${directoryToTest}
-php $base_dir/vendor/bin/php-cs-fixer fix ${directoryToTest}
-php $base_dir/vendor/bin/phpcbf --standard=${base_dir}vendor/magento/magento-coding-standard/Magento2/ruleset.xml ${directoryToTest}
-php $base_dir/vendor/bin/phpcs --standard=${base_dir}vendor/magento/magento-coding-standard/Magento2/ruleset.xml ${directoryToTest}
-php $base_dir/vendor/bin/phpmd ${directoryToTest} text ${base_dir}dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml
+err_report() {
+    echo "
+********************************************************
+QA Checks Failed - Please check the output and fix these
+********************************************************
+"
+}
+
+trap 'err_report $LINENO' ERR
+
+function usage(){
+    echo "
+
+Usage:
+    ./$0 [directoryToTest]
+
+    [directoryToTest] - The directory to test relative to root directory (default app/code)
+
+    "
+}
+
+if (( $# > 1 ))
+then
+    usage
+    exit 1
+fi
+
+directoryToTest=${1:-"app/code"}
+
+if [[ ${directoryToTest} == 'help' ]]
+then
+    usage
+    exit 1
+fi
+
+directoryToTest="${base_dir}${directoryToTest}"
+
+echo "
+---------------------------------
+Running CS fixer against the code
+---------------------------------
+"
+php $bin_dir/php-cs-fixer fix ${directoryToTest}
+echo "
+-----------------------------------
+Running Beautifier against the code
+-----------------------------------
+"
+set +e
+php $bin_dir/phpcbf \
+    --standard=${base_dir}vendor/magento/magento-coding-standard/Magento2/ruleset.xml \
+    ${directoryToTest} || true # BF return a non 0 exit code when it fixes things
+set -e
+echo "
+-------------------------------------
+Running Code Sniffer against the code
+-------------------------------------
+"
+php $bin_dir/phpcs \
+    --standard=${base_dir}vendor/magento/magento-coding-standard/Magento2/ruleset.xml \
+    ${directoryToTest}
+echo "
+--------------------------------------
+Running Mess Detector against the code
+--------------------------------------
+"
+php $bin_dir/phpmd ${directoryToTest} text ${base_dir}dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml
+echo "
+--------------------------------------------
+Running Copy Paste Detector against the code
+--------------------------------------------
+"
+php $bin_dir/phpcpd ${directoryToTest}
 #php $base_dir/bin/magento dev:tests:run static
+
+echo "
++++++++++++++++++++++++++
+QA Passed - Now run tests
++++++++++++++++++++++++++
+"

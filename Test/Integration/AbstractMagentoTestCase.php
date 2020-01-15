77ae99f73a1198c933942547c7de6ec0df91c5c8
Ross Mitchell <ross@edmondsommerce.co.uk>
2020-01-15 20:38:57 +0000

    Adding a couple of assertions that will probably be useful for others

--- Test/Integration/AbstractMagentoTestCase.php
+++ Test/Integration/AbstractMagentoTestCase.php
@@ -6,11 +6,13 @@ namespace EdmondsCommerce\Testing\Test\Integration;
 
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 use function get_class;
+use function thisCausesAnError;
 
 abstract class AbstractMagentoTestCase extends TestCase
 {
@@ -33,12 +35,28 @@ abstract class AbstractMagentoTestCase extends TestCase
     public function getProductBySku(string $sku): ProductInterface
     {
         /** @var ProductRepositoryInterface $repository */
-        $repository = $this->getObjectManager()->get(ProductRepositoryInterface::class);
+        $repository = $this->getObjectManager()->create(ProductRepositoryInterface::class);
         $product    = $repository->get($sku);
         if (!$product instanceof ProductInterface) {
             throw new RuntimeException('Expected ProductInterface, got ' . get_class($product));
         }
 
         return $product;
+    }
+    
+    protected function assertProductHasChanges(ProductInterface $product): void
+    {
+        if(!$product instanceof AbstractModel) {
+            throw new RuntimeException('Product must extend AbstractModel, got ' . get_class($product));
+        }
+        self::assertTrue($product->hasDataChanges());
+    }
+    
+    protected function assertProductHasNoChanges(ProductInterface $product): void
+    {
+        if(!$product instanceof AbstractModel) {
+            throw new RuntimeException('Product must extend AbstractModel, got ' . get_class($product));
+        }
+        self::assertFalse($product->hasDataChanges());
     }
 }

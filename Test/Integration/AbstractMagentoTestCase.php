5501ceff48e7f5436b92e4056a482e3cdf320e1a
Ross Mitchell <ross@edmondsommerce.co.uk>
2021-03-03 11:12:45 +0000

    Adding return type

--- Test/Integration/AbstractMagentoTestCase.php
+++ Test/Integration/AbstractMagentoTestCase.php
@@ -28,7 +28,7 @@ abstract class AbstractMagentoTestCase extends TestCase
      */
     private $objectMaanger;
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->objectMaanger = Bootstrap::getObjectManager();
